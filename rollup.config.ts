3ec5d91c0b7541c429387644e88a5537e326a439
Roman Faizullin <roman@faizullin.info>
2019-05-17 17:09:44 +0300

    Add file size to output, uglifier

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,6 +4,8 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import filesize from 'rollup-plugin-filesize'
+import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
@@ -13,12 +15,12 @@ export default {
   input: `src/${libraryName}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['react'],
   watch: {
-    include: 'src/**',
+    include: 'src/**'
   },
   plugins: [
     // Allow json resolution
@@ -34,5 +36,7 @@ export default {
 
     // Resolve source maps to the original source
     sourceMaps(),
-  ],
+    terser(),
+    filesize()
+  ]
 }
